af9e566ae8254cb06fb9325e205462508519c701
Alexander Wornast <tetracionist@gmail.com>
2024-05-17 21:43:06 +0100

    linted based on shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+``#!/bin/sh
 
 python3 -m venv .venv
 source .venv/bin/activate
@@ -13,18 +13,17 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 dbt clean && dbt deps
 
-sqlfluff lint --templater ${INPUT_SQLFLUFF_TEMPLATER} --dialect ${INPUT_SQLFLUFF_DIALECT} --disable-progress-bar . --format json > ${GITHUB_WORKSPACE}/lint_output.json
+sqlfluff lint --templater "${INPUT_SQLFLUFF_TEMPLATER}" --dialect "${INPUT_SQLFLUFF_DIALECT}" --disable-progress-bar . --format json > "${GITHUB_WORKSPACE}"/lint_output.json
 
 cd "${GITHUB_WORKSPACE}" || exit
 
 
 python -m json_to_rdjsonl --dbt_project_dir "${INPUT_DBT_PROJECT_DIR}" 
 
-cat < ${GITHUB_WORKSPACE}/"violations.rdjsonl"| reviewdog -f=rdjsonl \
-    -name="sqlfluff (sqlfluff-lint)" \
-    -reporter="${INPUT_REPORTER:-github-pr-check}" \
-    -filter-mode="${INPUT_FILTER_MODE}" \
-    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-    -level="${INPUT_LEVEL}" \
-    ${INPUT_REVIEWDOG_FLAGS}
-
+cat < "${GITHUB_WORKSPACE}"/"violations.rdjsonl"| reviewdog -f=rdjsonl \
+  -name="sqlfluff (sqlfluff-lint)" \
+  -reporter="${INPUT_REPORTER:-github-pr-check}" \
+  -filter-mode="${INPUT_FILTER_MODE}" \
+  -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+  -level="${INPUT_LEVEL}" \
+  "${INPUT_REVIEWDOG_FLAGS}"
