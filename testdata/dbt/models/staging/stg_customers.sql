8e7f90feb745cca3650e187dc3d588424d0a9d26
Alexander Wornast <tetracionist@gmail.com>
2024-05-17 17:25:21 +0100

    try breaking linting rules

--- testdata/dbt/models/staging/stg_customers.sql
+++ testdata/dbt/models/staging/stg_customers.sql
@@ -1,23 +1,5 @@
-with
+with source as (select * from {{ source('ecom', 'raw_customers') }}),
 
-source as (
-
-    select * from {{ source('ecom', 'raw_customers') }}
-
-),
-
-renamed as (
-
-    select
-
-        ----------  ids
-        id as customer_id,
-
-        ---------- text
-        name as customer_name
-
-    from source
-
-)
+renamed as (select id as customer_id, name as customer_name from source)
 
 select * from renamed
