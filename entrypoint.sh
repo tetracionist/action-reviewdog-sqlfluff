00c1e6607d08c4780113a15f60833541c86b4085
Alexander Wornast <tetracionist@gmail.com>
2024-05-17 21:59:28 +0100

    added sqlfluff mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,18 +13,41 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 dbt clean && dbt deps
 
-sqlfluff lint --templater ${INPUT_SQLFLUFF_TEMPLATER} --dialect ${INPUT_SQLFLUFF_DIALECT} --disable-progress-bar . --format json > ${GITHUB_WORKSPACE}/lint_output.json
 
-cd "${GITHUB_WORKSPACE}" || exit
+if [[ "${INPUT_SQLFLUFF_MODE}" == "lint" ]]; then
+  sqlfluff lint --templater ${INPUT_SQLFLUFF_TEMPLATER} --dialect ${INPUT_SQLFLUFF_DIALECT} --disable-progress-bar . --format json > ${GITHUB_WORKSPACE}/lint_output.json
 
+  cd "${GITHUB_WORKSPACE}" || exit
 
-python -m json_to_rdjsonl --dbt_project_dir "${INPUT_DBT_PROJECT_DIR}" 
+  python -m json_to_rdjsonl --dbt_project_dir "${INPUT_DBT_PROJECT_DIR}" 
 
-cat < ${GITHUB_WORKSPACE}/"violations.rdjsonl"| reviewdog -f=rdjsonl \
-    -name="sqlfluff (sqlfluff-lint)" \
-    -reporter="${INPUT_REPORTER:-github-pr-check}" \
-    -filter-mode="${INPUT_FILTER_MODE}" \
-    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-    -level="${INPUT_LEVEL}" \
-    ${INPUT_REVIEWDOG_FLAGS}
+  cat < ${GITHUB_WORKSPACE}/"violations.rdjsonl"| reviewdog -f=rdjsonl \
+      -name="sqlfluff (sqlfluff-lint)" \
+      -reporter="${INPUT_REPORTER:-github-pr-check}" \
+      -filter-mode="${INPUT_FILTER_MODE}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -level="${INPUT_LEVEL}" \
+      ${INPUT_REVIEWDOG_FLAGS}
+
+elif [[ "${INPUT_SQLFLUFF_MODE}" == "fix" ]]; then
+  sqlfluff fix --templater dbt --dialect snowflake --disable-progress-bar .
+
+  temp_file=$(mktemp)
+  git diff | tee "${temp_file}"
+  git stash -u
+
+  # shellcheck disable=SC2034
+  reviewdog \
+    -name="sqlfluff (sqlfluff-fix)" \
+    -f=diff \
+    -f.diff.strip=1 \
+    -reporter="${REVIEWDOG_REPORTER}" \
+    -filter-mode="${REVIEWDOG_FILTER_MODE}" \
+    -fail-on-error="${REVIEWDOG_FAIL_ON_ERROR}" \
+    -level="${REVIEWDOG_LEVEL}" <"${temp_file}" || exit_code=$?
+
+  # Clean up
+  git stash drop || true
+
+fi
 
