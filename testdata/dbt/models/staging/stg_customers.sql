1cd8178e23967794e00cc761955aeb776f45024c
Alexander Wornast <tetracionist@gmail.com>
2024-05-18 13:29:59 +0100

    fix linting

--- testdata/dbt/models/staging/stg_customers.sql
+++ testdata/dbt/models/staging/stg_customers.sql
@@ -1,6 +1,14 @@
-with source as (select * from {{ source('ecom', 'raw_customers') }}),
+with source as (
+    select * 
+    from {{ source('ecom', 'raw_customers') }}
+),
 
-renamed as (select id as customer_id, name as customer_name from source)
+renamed as (
+    select 
+        id as customer_id, 
+        name as customer_name 
+    from source
+)
 
 
 select * from renamed
