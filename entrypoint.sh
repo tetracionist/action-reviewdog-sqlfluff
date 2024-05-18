50348e3ad96039ee231caf2e3d51e10f50cc7e3e
Alexander Wornast <tetracionist@gmail.com>
2024-05-18 01:09:43 +0100

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ fi
 # create an environment variable that we can use to connect to Reviewdog
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo $${INPUT_DBT_PROFILES_DIR}""
+echo ${INPUT_DBT_PROFILES_DIR}""
 
 # install any dbt dependencies
 dbt clean && dbt deps --profiles-dir "${INPUT_DBT_PROFILES_DIR}" 
