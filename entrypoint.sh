6cc7c537e2eb59980c971d060aca2ed80a0e888e
Alexander Wornast <tetracionist@gmail.com>
2024-05-18 02:31:41 +0100

    try this

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 # use git to find any changed sql files
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
-git fetch --prune --unshallow --no-tags 
-changed_files=$(git diff -z --name-only --diff-filter=AM --relative="${INPUT_DBT_PROJECT_DIR}" "origin/${GITHUB_HEAD_REF}" "origin/${GITHUB_BASE_REF}"  -- '*.sql')
+git fetch --prune --unshallow --no-tags
+changed_files=$(cd "${INPUT_DBT_PROJECT_DIR}" && git diff -z --name-only --diff-filter=AM --relative="${INPUT_DBT_PROJECT_DIR}" "origin/${GITHUB_HEAD_REF}" "origin/${GITHUB_BASE_REF}"  -- '*.sql')
 if [ -z "$changed_files" ]; then
   echo "No SQL files changed or added"
   exit 0
