01523078925cf196a7ed4b1616787a22971c6849
Alexander Wornast <tetracionist@gmail.com>
2024-05-18 03:07:46 +0100

    need newline

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 
 git fetch --prune --unshallow --no-tags
-changed_files=$(cd "${INPUT_DBT_PROJECT_DIR}" && git diff -z --name-only --diff-filter=AM --relative \
+changed_files=$(cd "${INPUT_DBT_PROJECT_DIR}" && git diff --name-only --diff-filter=AM --relative \
   "origin/$GITHUB_HEAD_REF" "origin/$GITHUB_BASE_REF" -- '*.sql')
 
 
