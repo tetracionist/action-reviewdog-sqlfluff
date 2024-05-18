42983fc6ef0308414faf2500a73a0ec6436e20bc
Alexander Wornast <tetracionist@gmail.com>
2024-05-18 03:58:58 +0100

    okay bedtime

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,10 @@
 
 # use git to find any changed sql files
 git config --global --add safe.directory "${GITHUB_WORKSPACE}"
-git fetch origin $GITHUB_HEAD_REF
-git fetch origin $GITHUB_BASE_REF
+git fetch origin $GITHUB_HEAD_REF $GITHUB_BASE_REF
 
 changed_files=$(cd "${GITHUB_WORKSPACE}/${INPUT_DBT_PROJECT_DIR}" && git diff --name-only --diff-filter=AM --relative \
-  "origin/$GITHUB_BASE_REF" $GITHUB_HEAD_REF -- '*.sql')
+  "origin/$GITHUB_BASE_REF" "origin/$GITHUB_HEAD_REF" -- '*.sql')
 
 
 
